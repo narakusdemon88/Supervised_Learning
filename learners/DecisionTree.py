01bdb1710ccc65931cdf0a8e15179547de7bac11
jon <jakashi3@gatech.edu>
2023-02-04 18:12:44 +0900

    fixed DT

--- learners/DecisionTree.py
+++ learners/DecisionTree.py
@@ -1,190 +1,167 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split, learning_curve, StratifiedKFold
-from sklearn.metrics import f1_score
+from sklearn.model_selection import StratifiedKFold, learning_curve, train_test_split
+import numpy as np
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import f1_score
 from time import perf_counter
 import matplotlib.pyplot as plt
 
 
-def plot_results(title, list1, list2, ylabel, xlabel, list1_label, list2_label, dataset, algo=None, type=None):
-    x1, y1 = zip(*list1)
-    x2, y2 = zip(*list2)
+def plot_results(title, list_1, list_2, y_label, x_label, list_1_label, list_2_label, dataset, algo, type):
+    x1, y1 = zip(*list_1)
+    x2, y2 = zip(*list_2)
 
     plt.title(title)
-    plt.plot(x1, y1, label=list1_label)
-    plt.plot(x2, y2, label=list2_label)
-    plt.legend()
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
+    plt.plot(x1, y1, label=list_1_label)
+    plt.plot(x2, y2, label=list_2_label)
 
+    plt.legend()
+    plt.xlabel(xlabel=x_label)
+    plt.ylabel(ylabel=y_label)
     plt.grid()
-    if algo is not None and type is not None:
-        plt.savefig(f"../images/{dataset}/{algo}/cross_validation_{type}.png")
+    plt.savefig(f"../images/{dataset}/{algo}/{title}.png")
     plt.show()
 
-
-def calculate_cross_val_score(X_train, y_train, dataset, max_depth=None, min_leaf=None, algo=None, type=None):
-    """
-    :param X_train:
-    :param y_train:
-    :param dataset:
-    :param max_depth:
-    :param min_leaf:
-    """
-    if max_depth is None and min_leaf is None:
-        # working with the default tree (no hyperparameter tuning)
-        clf = DecisionTreeClassifier()
-    elif max_depth is not None and min_leaf is None:
-        # tweaking just max depth
-        clf = DecisionTreeClassifier(max_depth=max_depth)
-    elif max_depth is None and min_leaf is not None:
-        # tweaking just min leaf
-        clf = DecisionTreeClassifier(min_samples_leaf=min_leaf)
-    else:
-        # tweaking both
-        clf = DecisionTreeClassifier(max_depth=max_depth, min_samples_leaf=min_leaf)
-
-    training_sizes, training_scores, test_scores = learning_curve(
-        clf,
-        X_train,
-        y_train,
-        cv=10
-    )
-
-    # titanic --> Titanic
-    dataset_upper = dataset[0].upper() + dataset[1:]
-
-    training_sizes_percents = [(i/len(X_train))*100 for i in training_sizes]
-
-    plt.plot(training_sizes_percents, training_scores.mean(axis=1), label="Training Score")
-    plt.plot(training_sizes_percents, test_scores.mean(axis=1), label="Cross-Validation Score")
-    plt.legend()
-    plt.xlabel("Sample Size %")
-    plt.ylabel("F1 Score")
-    plt.title(f"Learning Curve: {dataset_upper} ({type})")
-    plt.grid()
-    if algo is not None and type is not None:
-        plt.savefig(f"../images/{dataset}/{algo}/cross_validation_{type}.png")
-    plt.show()
 
 def main():
-    datasets = ["titanic", "winequality-red"]
-    # datasets = ["winequality-red"]
-
-    for dataset in datasets:
-        print(f"\nProcessing {dataset.upper()}")
-        df = pd.read_csv(f"../datasets/{dataset}.csv")
+    for dataset in ["titanic", "winequality-red"]:
+        print(f"Processing {dataset}")
+        data = pd.read_csv(f"../datasets/{dataset}.csv")
 
         if dataset == "titanic":
             predict_col = "Survived"
         else:
             predict_col = "quality"
 
-        X = df.drop([predict_col], axis=1)
-        y = df[predict_col]
+        X = data.drop(columns=[predict_col])
+        y = data[predict_col]
 
         X = pd.get_dummies(X)
         X.fillna(X.mean(), inplace=True)
+        k_folds = StratifiedKFold(n_splits=5)
 
-        splits = 10
-        k_folds = StratifiedKFold(n_splits=splits)
+        for hyperparameter in ["max_depth", "min_leaf"]:
+            f1_scores = []
+            f1_scores_train = []
+            fit_times = []
+            pred_times = []
 
+            for i in [i for i in range(1, 21, 1)]:
+
+                if hyperparameter == "max_depth":
+                    dt = DecisionTreeClassifier(max_depth=i)
+                else:
+                    dt = DecisionTreeClassifier(min_samples_leaf=i)
+
+                fold_f1_scores_test = []
+                fold_f1_scores_train = []
+                fold_fit_times = []
+                fold_pred_times = []
+
+                k_fold_split = k_folds.split(X, y)
+
+                for train_i, test_i in k_fold_split:
+
+                    X_train = X.iloc[train_i]
+                    X_test = X.iloc[test_i]
+                    y_train = y.iloc[train_i]
+                    y_test = y.iloc[test_i]
+
+                    t1 = perf_counter()
+                    dt.fit(X_train, y_train)
+                    t2 = perf_counter()
+
+                    y_pred = dt.predict(X_test)
+                    t3 = perf_counter()
+
+                    y_pred_train = dt.predict(X_train)
+
+                    fit_time = t2 - t1
+                    pred_time = t3 - t2
+
+                    fold_f1_score_test = f1_score(y_test, y_pred, average="weighted")
+                    fold_f1_score_train = f1_score(y_train, y_pred_train, average="weighted")
+
+                    # append times to lists
+                    fold_f1_scores_test.append(fold_f1_score_test)
+                    fold_f1_scores_train.append(fold_f1_score_train)
+                    fold_fit_times.append(fit_time)
+                    fold_pred_times.append(pred_time)
+
+                average_f1_score = sum(fold_f1_scores_test) / len(fold_f1_scores_test)
+                average_f1_score_train = sum(fold_f1_scores_train) / len(fold_f1_scores_train)
+                average_fit_time = sum(fold_fit_times) / len(fold_fit_times)
+                average_pred_time = sum(fold_pred_times) / len(fold_pred_times)
+
+                f1_scores.append((i, average_f1_score))
+                f1_scores_train.append((i, average_f1_score_train))
+
+                fit_times.append((i, average_fit_time))
+                pred_times.append((i, average_pred_time))
+
+                print(f"{i} F1:{average_f1_score}, Fit:{average_fit_time}, Pred:{average_pred_time}")
+            print(f"f1 scores for each {hyperparameter} value: ", f1_scores)
+
+            # PLOT F1 SCORES
+            plot_results(
+                title=f"Decision Tree F1 Score for {hyperparameter} on {dataset}",
+                list_1=f1_scores,
+                list_2=f1_scores_train,
+                x_label=hyperparameter,
+                y_label="F1 Score",
+                list_1_label="Test Score",
+                list_2_label="Train Score",
+                dataset=dataset,
+                algo="dt",
+                type=f"{hyperparameter}_f1")
+            # PLOT TIMES
+            plot_results(
+                title=f"Decision Tree Times for {hyperparameter} on {dataset}",
+                list_1=fit_times,
+                list_2=pred_times,
+                x_label=hyperparameter,
+                y_label="Time (Seconds)",
+                list_1_label="Train Times",
+                list_2_label="Predict Times",
+                dataset=dataset,
+                algo="dt",
+                type=f"{hyperparameter}_times")
+
+        # PLOT DEFAULT TREE
         X_train, X_test, y_train, y_test = train_test_split(
             X,
             y,
             random_state=0,
             test_size=0.2,
-            shuffle=True)
-
-        # List of hyperparameters
-        hyperparameters = ["max_depth", "min_leaf"]
-
-        for hyperparameter in hyperparameters:
-            print(f"Testing {hyperparameter}")
-
-            train_time = []
-            predict_time = []
-
-            f1_test_scores = []
-            f1_train_scores = []
-            for i in range(1, 21):
-                if hyperparameter == "max_depth":
-                    clf = DecisionTreeClassifier(max_depth=i)
+            shuffle=True
+        )
+        for tree in ["default", "optimized"]:
+            if tree == "default":
+                dt = DecisionTreeClassifier()
+            else:
+                if dataset == "titanic":
+                    dt = DecisionTreeClassifier(max_depth=6, min_samples_leaf=5)
                 else:
-                    clf = DecisionTreeClassifier(min_samples_leaf=i)
+                    dt = DecisionTreeClassifier(max_depth=3, min_samples_leaf=1)
 
-                f1_test_fold_scores = []
-                f1_train_fold_scores = []
+            train_sizes, train_scores, test_scores = learning_curve(
+                estimator=dt,
+                X=X_train,
+                y=y_train,
+                cv=10)
 
-                test_fold_times = []
-                train_fold_times = []
+            train_scores_average = np.mean(train_scores, axis=1)
+            test_scores_average = np.mean(test_scores, axis=1)
 
-                for train_i, test_i in k_folds.split(X, y):
-                    X_train = X.iloc[train_i]
-                    X_test = X.iloc[test_i]
-
-                    y_train = y.iloc[train_i]
-                    y_test = y.iloc[test_i]
-
-                    # time the fit and predict time
-                    time1 = perf_counter()
-                    clf.fit(X_train, y_train)
-                    time2 = perf_counter()
-                    y_pred_test = clf.predict(X_test)
-                    time3 = perf_counter()
-
-
-                t1 = perf_counter()
-                clf.fit(X_train, y_train)
-                t2 = perf_counter()
-                y_pred_test = clf.predict(X_test)
-                t3 = perf_counter()
-
-                train_time.append((i, t2 - t1))
-                predict_time.append((i, t3 - t2))
-
-                # y_pred_test = clf.predict(X_test)
-                y_pred_train = clf.predict(X_train)
-
-                f1_test = f1_score(y_test, y_pred_test, average="weighted")
-                f1_train = f1_score(y_train, y_pred_train, average="weighted")
-                f1_test_scores.append((i, f1_test))
-                f1_train_scores.append((i, f1_train))
-
-                print(f"{hyperparameter}: {i}, F1_Score: {f1_test, f1_train}")
-
-            plot_results(
-                title=f"Decision Tree F1 Score for {hyperparameter[0].upper() + hyperparameter[1:]} on {dataset}",
-                list1=f1_test_scores,
-                list2=f1_train_scores,
-                xlabel=hyperparameter,
-                ylabel="F1 Score",
-                list1_label="Test Score",
-                list2_label="Train Score",
-                dataset=dataset,
-                algo="dt",
-                type=hyperparameter)
-
-            plot_results(
-                title=f"Decision Tree Time for {hyperparameter[0].upper() + hyperparameter[1:]} on {dataset}",
-                list1=train_time,
-                list2=predict_time,
-                xlabel=hyperparameter,
-                ylabel="Time (Seconds)",
-                list1_label="Train Time",
-                list2_label="Predict Time",
-                dataset=dataset,
-                algo="dt",
-                type=hyperparameter)
-
-        # test the default tree
-        calculate_cross_val_score(X_train=X_train, y_train=y_train, dataset=dataset, algo="dt", type="default")
-
-        # test the optimized tree
-        if dataset == "titanic":
-            calculate_cross_val_score(X_train=X_train, y_train=y_train, dataset=dataset, max_depth=6, min_leaf=5, algo="dt", type="optimized")
-        else:
-            calculate_cross_val_score(X_train=X_train, y_train=y_train, dataset=dataset, max_depth=3, min_leaf=1, algo="dt", type="optimized")
+            plt.plot(train_sizes, train_scores_average, label="Training Score")
+            plt.plot(train_sizes, test_scores_average, label="CV Score")
+            plt.title(f"Learning Curve for {tree} on {dataset}")
+            plt.xlabel("# of Samples")
+            plt.ylabel("Performance Score")
+            plt.legend()
+            plt.grid()
+            plt.savefig(f"../images/{dataset}/dt/Learning Curve for {tree} on {dataset}.png")
+            plt.show()
 
 
 if __name__ == "__main__":
