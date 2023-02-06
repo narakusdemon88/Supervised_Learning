7efd325286946d086204b284a0e23cfd87eb2c6e
Jon <narakusdemon88@gmail.com>
2023-02-05 20:54:55 -0800

    Update Adaboost.py

--- learners/Adaboost.py
+++ learners/Adaboost.py
@@ -1,208 +1,247 @@
-from time import perf_counter
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import StratifiedKFold, learning_curve, train_test_split
+import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.model_selection import train_test_split, learning_curve
 from sklearn.metrics import f1_score
-from DecisionTree import plot_results
-import numpy as np
+from time import perf_counter
 import matplotlib.pyplot as plt
 
 
-def calculate_cross_val_score(X_train, y_train, dataset, weak_learners, learning_rate, algo=None, type=None):
-    """
-    :param X_train:
-    :param y_train:
-    :param dataset:
-    :param weak_learners:
-    :param learning_rate:
-    :return:
-    """
-    if weak_learners is None and learning_rate is None:
-        # working with the default tree (no hyperparameter tuning)
-        clf = AdaBoostClassifier()
-    elif weak_learners is not None and learning_rate is None:
-        # tweaking just max depth
-        clf = AdaBoostClassifier(n_estimators=weak_learners)
-    elif weak_learners is None and learning_rate is not None:
-        # tweaking just min leaf
-        clf = AdaBoostClassifier(learning_rate=learning_rate)
-    else:
-        # tweaking both
-        clf = AdaBoostClassifier(n_estimators=weak_learners, learning_rate=learning_rate)
+def plot_results(title, list_1, list_2, y_label, x_label, list_1_label, list_2_label, dataset, algo, type):
+    x1, y1 = zip(*list_1)
+    x2, y2 = zip(*list_2)
 
-    training_sizes, training_scores, test_scores = learning_curve(
-        clf,
-        X_train,
-        y_train,
-        cv=10
-    )
+    plt.title(title)
+    plt.plot(x1, y1, label=list_1_label)
+    plt.plot(x2, y2, label=list_2_label)
 
-    # titanic --> Titanic
-    dataset_upper = dataset[0].upper() + dataset[1:]
-
-    training_sizes_percents = [(i / len(X_train)) * 100 for i in training_sizes]
-
-    plt.plot(training_sizes_percents, training_scores.mean(axis=1), label="Training Score")
-    plt.plot(training_sizes_percents, test_scores.mean(axis=1), label="Cross-Validation Score")
     plt.legend()
-    plt.xlabel("Sample Size %")
-    plt.ylabel("F1 Score")
-    plt.title(f"Learning Curve: {dataset_upper}")
+    plt.xlabel(xlabel=x_label)
+    plt.ylabel(ylabel=y_label)
     plt.grid()
-    if algo is not None and type is not None:
-        plt.savefig(f"../images/{dataset}/{algo}/cross_validation_{type}.png")
+    plt.savefig(f"../images/{dataset}/{algo}/{title}.png")
     plt.show()
 
 
 def main():
     for dataset in ["titanic", "winequality-red"]:
         print(f"Processing {dataset}")
-
-        df = pd.read_csv(f"../datasets/{dataset}.csv")
+        data = pd.read_csv(f"../datasets/{dataset}.csv")
 
         if dataset == "titanic":
             predict_col = "Survived"
-        else:  # wine quality dataset
+        else:
             predict_col = "quality"
 
-        X = df.drop([predict_col], axis=1)
-        y = df[predict_col]
+        X = data.drop(columns=[predict_col])
+        y = data[predict_col]
 
         X = pd.get_dummies(X)
         X.fillna(X.mean(), inplace=True)
+        k_folds = StratifiedKFold(n_splits=5)
 
-        X_train, X_test, y_train, y_test = train_test_split(
-            X,
-            y,
-            random_state=0,
-            test_size=0.2,
-            shuffle=True)
+        for hyperparameter in ["weak_learners", "learning_rate"]:
+            f1_scores = []
+            f1_scores_train = []
+            fit_times = []
+            pred_times = []
 
-        # test hyperparamaters. n_estimators = # of weak learners
-        # learning_rate = learning rate
-        # base_estimator
-        for hyperparameter in ["# of weak learners", "learning rate"]:
-            if hyperparameter == "# of weak learners":
-                # weak_learners_range = [i for i in range(1, 101, 1)]
+            if hyperparameter == "weak_learners":
+                for i in [i for i in range(1, 101, 1)]:
+                    boost = AdaBoostClassifier(estimator=DecisionTreeClassifier(max_depth=1), n_estimators=i)
 
-                train_time = []
-                predict_time = []
+                    fold_f1_scores_test = []
+                    fold_f1_scores_train = []
+                    fold_fit_times = []
+                    fold_pred_times = []
 
-                f1_test_scores = []
-                f1_train_scores = []
+                    k_fold_split = k_folds.split(X, y)
 
-                for weak_learner_number in [i for i in range(1, 101, 1)]:
+                    for train_i, test_i in k_fold_split:
+                        X_train = X.iloc[train_i]
+                        X_test = X.iloc[test_i]
+                        y_train = y.iloc[train_i]
+                        y_test = y.iloc[test_i]
 
-                    clf = AdaBoostClassifier(estimator=DecisionTreeClassifier(max_depth=1), n_estimators=weak_learner_number)
-                    t1 = perf_counter()
-                    clf.fit(X_train, y_train)
-                    t2 = perf_counter()
-                    y_pred_test = clf.predict(X_test)
-                    t3 = perf_counter()
+                        t1 = perf_counter()
+                        boost.fit(X_train, y_train)
+                        t2 = perf_counter()
 
-                    train_time.append((weak_learner_number, t2 - t1))
-                    predict_time.append((weak_learner_number, t3 - t2))
+                        y_pred = boost.predict(X_test)
+                        t3 = perf_counter()
 
-                    # y_pred_test = clf.predict(X_test)
-                    y_pred_train = clf.predict(X_train)
+                        y_pred_train = boost.predict(X_train)
 
-                    f1_test = f1_score(y_test, y_pred_test, average="weighted")
-                    f1_train = f1_score(y_train, y_pred_train, average="weighted")
-                    f1_test_scores.append((weak_learner_number, f1_test))
-                    f1_train_scores.append((weak_learner_number, f1_train))
+                        fit_time = t2 - t1
+                        pred_time = t3 - t2
 
-                    print(f"{hyperparameter}: {weak_learner_number}, F1_Score: {f1_test, f1_train}")
+                        fold_f1_score_test = f1_score(y_test, y_pred, average="weighted")
+                        fold_f1_score_train = f1_score(y_train, y_pred_train, average="weighted")
 
+                        # append times to lists
+                        fold_f1_scores_test.append(fold_f1_score_test)
+                        fold_f1_scores_train.append(fold_f1_score_train)
+                        fold_fit_times.append(fit_time)
+                        fold_pred_times.append(pred_time)
+
+                    average_f1_score = sum(fold_f1_scores_test) / len(fold_f1_scores_test)
+                    average_f1_score_train = sum(fold_f1_scores_train) / len(fold_f1_scores_train)
+                    average_fit_time = sum(fold_fit_times) / len(fold_fit_times)
+                    average_pred_time = sum(fold_pred_times) / len(fold_pred_times)
+
+                    f1_scores.append((i, average_f1_score))
+                    f1_scores_train.append((i, average_f1_score_train))
+
+                    fit_times.append((i, average_fit_time))
+                    pred_times.append((i, average_pred_time))
+
+                    print(f"{i} F1:{average_f1_score}, Fit:{average_fit_time}, Pred:{average_pred_time}")
+                print(f"f1 scores for each {hyperparameter} value: ", f1_scores)
+
+                # PLOT F1 SCORES
                 plot_results(
-                    title=f"SVM F1 Score for {hyperparameter[0].upper() + hyperparameter[1:]} on {dataset}",
-                    list1=f1_test_scores,
-                    list2=f1_train_scores,
-                    xlabel=hyperparameter,
-                    ylabel="F1 Score",
-                    list1_label="Test Score",
-                    list2_label="Train Score",
+                    title=f"Adaboost F1 Score for {hyperparameter} on {dataset}",
+                    list_1=f1_scores,
+                    list_2=f1_scores_train,
+                    x_label=hyperparameter,
+                    y_label="F1 Score",
+                    list_1_label="Test Score",
+                    list_2_label="Train Score",
                     dataset=dataset,
                     algo="boost",
-                    type=hyperparameter)
-
+                    type=f"{hyperparameter}_f1")
+                # PLOT TIMES
                 plot_results(
-                    title=f"SVM Time for {hyperparameter[0].upper() + hyperparameter[1:]} on {dataset}",
-                    list1=train_time,
-                    list2=predict_time,
-                    xlabel=hyperparameter,
-                    ylabel="Time (Seconds)",
-                    list1_label="Train Time",
-                    list2_label="Predict Time",
+                    title=f"Adaboost Times for {hyperparameter} on {dataset}",
+                    list_1=fit_times,
+                    list_2=pred_times,
+                    x_label=hyperparameter,
+                    y_label="Time (Seconds)",
+                    list_1_label="Train Times",
+                    list_2_label="Predict Times",
                     dataset=dataset,
                     algo="boost",
-                    type=hyperparameter)
+                    type=f"{hyperparameter}_times")
 
             else:
-                # hyperparameter == "learning rate"
-                train_time = []
-                predict_time = []
+                # hyperparameter = C
+                for i in np.linspace(0.000001, 1, 100):
+                    boost = AdaBoostClassifier(estimator=DecisionTreeClassifier(max_depth=1), learning_rate=i)
 
-                f1_test_scores = []
-                f1_train_scores = []
+                    fold_f1_scores_test = []
+                    fold_f1_scores_train = []
+                    fold_fit_times = []
+                    fold_pred_times = []
 
-                for rate in np.linspace(0.000001, 1, 100):
-                    clf = AdaBoostClassifier(estimator=DecisionTreeClassifier(max_depth=1), learning_rate=rate)
-                    t1 = perf_counter()
-                    clf.fit(X_train, y_train)
-                    t2 = perf_counter()
-                    y_pred_test = clf.predict(X_test)
-                    t3 = perf_counter()
+                    k_fold_split = k_folds.split(X, y)
 
-                    train_time.append((rate, t2 - t1))
-                    predict_time.append((rate, t3 - t2))
+                    for train_i, test_i in k_fold_split:
+                        X_train = X.iloc[train_i]
+                        X_test = X.iloc[test_i]
+                        y_train = y.iloc[train_i]
+                        y_test = y.iloc[test_i]
 
-                    # y_pred_test = clf.predict(X_test)
-                    y_pred_train = clf.predict(X_train)
+                        t1 = perf_counter()
+                        boost.fit(X_train, y_train)
+                        t2 = perf_counter()
 
-                    f1_test = f1_score(y_test, y_pred_test, average="weighted")
-                    f1_train = f1_score(y_train, y_pred_train, average="weighted")
-                    f1_test_scores.append((rate, f1_test))
-                    f1_train_scores.append((rate, f1_train))
+                        y_pred = boost.predict(X_test)
+                        t3 = perf_counter()
 
-                    print(f"{hyperparameter}: {rate}, F1_Score: {f1_test, f1_train}")
+                        y_pred_train = boost.predict(X_train)
 
+                        fit_time = t2 - t1
+                        pred_time = t3 - t2
+
+                        fold_f1_score_test = f1_score(y_test, y_pred, average="weighted")
+                        fold_f1_score_train = f1_score(y_train, y_pred_train, average="weighted")
+
+                        # append times to lists
+                        fold_f1_scores_test.append(fold_f1_score_test)
+                        fold_f1_scores_train.append(fold_f1_score_train)
+                        fold_fit_times.append(fit_time)
+                        fold_pred_times.append(pred_time)
+
+                    average_f1_score = sum(fold_f1_scores_test) / len(fold_f1_scores_test)
+                    average_f1_score_train = sum(fold_f1_scores_train) / len(fold_f1_scores_train)
+                    average_fit_time = sum(fold_fit_times) / len(fold_fit_times)
+                    average_pred_time = sum(fold_pred_times) / len(fold_pred_times)
+
+                    f1_scores.append((i, average_f1_score))
+                    f1_scores_train.append((i, average_f1_score_train))
+
+                    fit_times.append((i, average_fit_time))
+                    pred_times.append((i, average_pred_time))
+
+                    print(f"{i} F1:{average_f1_score}, Fit:{average_fit_time}, Pred:{average_pred_time}")
+                print(f"f1 scores for each {hyperparameter} value: ", f1_scores)
+
+                # PLOT F1 SCORES
                 plot_results(
-                    title=f"SVM F1 Score for {hyperparameter[0].upper() + hyperparameter[1:]} on {dataset}",
-                    list1=f1_test_scores,
-                    list2=f1_train_scores,
-                    xlabel=hyperparameter,
-                    ylabel="F1 Score",
-                    list1_label="Test Score",
-                    list2_label="Train Score",
+                    title=f"Adaboost F1 Score for {hyperparameter} on {dataset}",
+                    list_1=f1_scores,
+                    list_2=f1_scores_train,
+                    x_label=hyperparameter,
+                    y_label="F1 Score",
+                    list_1_label="Test Score",
+                    list_2_label="Train Score",
                     dataset=dataset,
                     algo="boost",
-                    type=hyperparameter)
-
+                    type=f"{hyperparameter}_f1")
+                # PLOT TIMES
                 plot_results(
-                    title=f"SVM Time for {hyperparameter[0].upper() + hyperparameter[1:]} on {dataset}",
-                    list1=train_time,
-                    list2=predict_time,
-                    xlabel=hyperparameter,
-                    ylabel="Time (Seconds)",
-                    list1_label="Train Time",
-                    list2_label="Predict Time",
+                    title=f"Adaboost Times for {hyperparameter} on {dataset}",
+                    list_1=fit_times,
+                    list_2=pred_times,
+                    x_label=hyperparameter,
+                    y_label="Time (Seconds)",
+                    list_1_label="Train Times",
+                    list_2_label="Predict Times",
                     dataset=dataset,
                     algo="boost",
-                    type=hyperparameter)
+                    type=f"{hyperparameter}_times")
 
-        # test the default tree
-        calculate_cross_val_score(
-            X_train=X_train, y_train=y_train, dataset=dataset, weak_learners=46, learning_rate=0.01, algo="boost", type="default")
+                # PLOT DEFAULT TREE
+                X_train, X_test, y_train, y_test = train_test_split(
+                    X,
+                    y,
+                    random_state=0,
+                    test_size=0.2,
+                    shuffle=True
+                )
+                # Scale the data down so it runs
+                scaler = StandardScaler()
+                X_train = scaler.fit_transform(X_train)
 
+                for tree in ["default", "optimized"]:
+                    if tree == "default":
+                        boost = AdaBoostClassifier()
+                    else:
+                        if dataset == "titanic":
+                            boost = AdaBoostClassifier(estimator=DecisionTreeClassifier(max_depth=1), learning_rate=0.000001, n_estimators=1)
+                        else:
+                            boost = AdaBoostClassifier(estimator=DecisionTreeClassifier(max_depth=1), learning_rate=0.0094, n_estimators=46)
 
-        # test the optimized tree
-        if dataset == "titanic":
-            calculate_cross_val_score(
-                X_train=X_train, y_train=y_train, dataset=dataset, weak_learners=1, learning_rate=0.000001, algo="boost", type="optimized")
-        else:
-            calculate_cross_val_score(
-                X_train=X_train, y_train=y_train, dataset=dataset, weak_learners=46, learning_rate=0.01, algo="boost", type="optimized")
+                    train_sizes, train_scores, test_scores = learning_curve(
+                        estimator=boost,
+                        X=X_train,
+                        y=y_train,
+                        cv=10)
+
+                    train_scores_average = np.mean(train_scores, axis=1)
+                    test_scores_average = np.mean(test_scores, axis=1)
+
+                    plt.plot(train_sizes, train_scores_average, label="Training Score")
+                    plt.plot(train_sizes, test_scores_average, label="CV Score")
+                    plt.title(f"Learning Curve for {tree} on {dataset}")
+                    plt.xlabel("# of Samples")
+                    plt.ylabel("Performance Score")
+                    plt.legend()
+                    plt.grid()
+                    plt.savefig(f"../images/{dataset}/boost/Learning Curve for {tree} on {dataset}.png")
+                    plt.show()
 
 
 if __name__ == "__main__":
